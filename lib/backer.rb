953b849365c45843f5564a1fc0a66d3f72316d46
zhenggao cai <caizhengao1992@gmail.com>
2019-05-25 05:53:57 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+class Backer
+  attr_accessor :project
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def name 
+    @name
+  end
+  def backed_projects
+    @backed_projects
+  end
+  
+  def back_project(project)
+    @backed_projects << project 
+    #Project.backers << project 
+  end
+  
+end
---
+++ lib/project.rb
@@ -0,0 +1,21 @@
+class Project 
+  attr_accessor :backer
+  def initialize(title)
+    @title = title 
+    @backers = []
+  end
+  
+  def title 
+    @title
+  end
+  
+  def backers
+    @backers
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+   # Backer.backed_projects << backer
+  end
+  
+end
